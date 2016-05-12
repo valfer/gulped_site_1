8c508c120300ade775473a1e343a48bf7970784e
Valerio Ferrucci <valerio.ferrucci@me.com>
2016-05-12 15:03:25 +0200

    first import

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+
+// instantiate the helpers
+var concat = require('gulp-concat');
+var cleanCSS = require('gulp-clean-css');
+
+// list of files source fo the task
+var css_files = [
+ 'css/base.css',
+ 'css/dark-theme.css',
+ 'css/layout.css',
+ 'css/module.css',
+ 'css/state.css'
+];
+
+// the task
+gulp.task('default', function() {
+ return gulp.src(css_files)	// the source
+ .pipe(cleanCSS())
+ .pipe(concat('all.css'))	// concat
+ .pipe(gulp.dest('css'))	// output to this folder
+});
