b2d77573463a466aa45b4c73d595755b6caacd10
Valerio Ferrucci <valerio.ferrucci@me.com>
2016-05-12 15:32:50 +0200

    check files

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 // instantiate the helpers
 var concat = require('gulp-concat');
 var cleanCSS = require('gulp-clean-css');
+var fs = require('fs');
 
 // list of files source for the task
 var css_files = [
@@ -14,9 +15,24 @@ var css_files = [
 ];
 
 // the task
-gulp.task('default', function() {
+checkFiles(css_files);
+gulp.task('css_build', function() {
  return gulp.src(css_files)	// the source
  .pipe(cleanCSS())
  .pipe(concat('all.css'))	// concat
  .pipe(gulp.dest('css'))	// output to this folder
 });
+
+//===== utils
+function checkFiles(pathsList) {
+
+    var totPaths = pathsList.length;
+    for (var i = 0; i < totPaths; i++) {
+        var path = pathsList[i];
+        fs.stat(path, function (err, stat) {
+            if (err != null) {
+                console.log("ERROR ON PATH: '" + err.path + "' ----> " + err.code);
+            }
+        });
+    }
+}
